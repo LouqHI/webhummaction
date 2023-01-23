fe341099c73174699a3e2d24666e386466cbed52
unknown <michaelandryjohnson@gmail.com>
2023-01-23 16:00:10 +0100

    Ajout des routes API

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,23 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+use App\Repository;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
-    
     // Les deux fonctions renvoient désormait un JSON.
-    public function homeIndex()
-    {   
-        $user = array("id" => "1", "name" => "user 1");
-        return $this->JSONResponse($user);
+    public function homeIndex(PersistenceManagerRegistry $doctrine)
+    {        
+        $user = $doctrine->getRepository(User::class)->find("1");
+        return $this->JSON($user);
     }
 
     public function homeDeuxIndex()
     {
         $user = array("id" => "2", "name" => "user 2");
-        return $this->JSONResponse($user);
+        return $this->JSON($user);
     }
-
-
 
 }
