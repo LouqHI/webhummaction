a45c2789e4e07c7612802e43a56b10d31f939bcc
LouqHI <h.islamiya75@gmail.com>
2023-01-25 04:26:55 +0100

    remplacement de vuetify project par nuxt pour le dossier front

---
+++ front/app.vue
@@ -0,0 +1,11 @@
+<script setup>
+const {data:user}= await useFetch('http://localhost:3200/api/user2')
+
+</script>
+
+<template>
+  <div>
+    <h1>Entrée de l'application</h1>
+    <h2>{{ user }}</h2>
+  </div>
+</template>
